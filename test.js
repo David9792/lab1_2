ee5f6f4af9b7e43325245b1add8ded3d1fdc0618
david9792 <davit.el.unicodj@gmail.com>
2020-09-23 22:04:14 -0400

    version1

--- test.js
+++ test.js
@@ -7,6 +7,5 @@ let p3 = new Products("Pollo", "Pollo Imba", 50);
 let manager = new ProductsManagement();
 manager.addProducts(p1);
 manager.addProducts(p2);
-//manager.removeProducts(p1);
 manager.updateProducts(p1, p3);
 console.log(manager.showProducts());
